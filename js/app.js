e31881226a45eef07c3ebee76ef7fb99c8661993
prometeo88 <94573686+prometeo88@users.noreply.github.com>
2023-02-23 23:20:39 -0300

    add function y optimice el codigo

--- js/app.js
+++ js/app.js
@@ -3,6 +3,35 @@
 console.log("Bienvenido, vamos a cotizar el seguro de tu vehículo. Responde las preguntas con las opciones indicadas. Gracias")
 document.write("Bienvenido, vamos a cotizar el seguro de tu vehículo.", "<br>", "Responde las preguntas con las opciones indicadas. Gracias", "<br><br>")
 
+//funcion para cotizar
+
+function cotizarSeguro(){
+    let autooMoto = prompt("¿Quiere cotizar un auto o una moto? auto/moto");
+    if (autooMoto == "auto") {
+        let tercerosAuto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia");
+        if (tercerosAuto == "terceros") {
+            console.log("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
+            document.write("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
+        }
+        if (tercerosAuto == "amplia") {
+            console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+            document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+        }
+    }
+    if (autooMoto == "moto") {
+        let tercerosMoto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia")
+        if (tercerosMoto == "terceros") {
+            console.log("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
+            document.write("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
+        }
+        if (tercerosMoto == "amplia") {
+            console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+            document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+        }
+    }
+
+}
+
 //preguntas al usuario que tienen seguro
 
 let tieneSeguro
@@ -15,30 +44,7 @@ if (tieneSeguro == "si") {
     if (quiereComparar == "no") {
         alert("Gracias por visitarnos! Dudas comentarios al 3854855796");
     }
-    if (quiereComparar == "si") {
-        let autooMoto = prompt("¿Quiere cotizar un auto o una moto? auto/moto");
-        if (autooMoto == "auto") {
-            let tercerosAuto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia");
-            if (tercerosAuto == "terceros") {
-                console.log("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
-                document.write("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
-            }
-            if (tercerosAuto == "amplia") {
-                console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-                document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            }
-        }
-        if (autooMoto == "moto") {
-            let tercerosMoto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia")
-            if (tercerosMoto == "terceros") {
-                console.log("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
-                document.write("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
-            }
-            if (tercerosMoto == "amplia") {
-                console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-                document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            }
-        }
+    if (quiereComparar == "si") { cotizarSeguro()
     }
 }
 
@@ -49,35 +55,15 @@ else if (tieneSeguro == "no") {
     if (quiereComparar == "no") {
         alert("Gracias por visitarnos! Dudas comentarios al 3854855796");
     }
-    if (quiereComparar == "si") {
-        let autooMoto = prompt("¿Quiere cotizar un auto o una moto? auto/moto");
-        if (autooMoto == "auto") {
-            let tercerosAuto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia");
-            if (tercerosAuto == "terceros") {
-                console.log("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
-                document.write("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
-            }
-            if (tercerosAuto == "amplia") {
-                console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-                document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            }
-        }
-        if (autooMoto == "moto") {
-            let tercerosMoto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia")
-            if (tercerosMoto == "terceros") {
-                console.log("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
-                document.write("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
-            }
-            if (tercerosMoto == "amplia") {
-                console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-                document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            }
-        }
+    if (quiereComparar == "si") { cotizarSeguro()
     }
 } else {
     console.log("Lo siento, no puedo entender su respuesta. Por favor, responda con las opciones permitidas");
     document.write("<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con las opciones permitidas")
 }
+
+
+
 
 
 
