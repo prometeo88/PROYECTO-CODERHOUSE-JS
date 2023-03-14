ab75f596f59395e73835edd6e6378e40e5035e75
Federico <f.ullua@gmail.com>
2023-03-14 17:48:08 -0300

    agrego objetos y optimizo codigo

--- js/app.js
+++ js/app.js
@@ -1,66 +1,130 @@
 // Mensajes Bienvenida
 
 console.log("Bienvenido, vamos a cotizar el seguro de tu vehículo. Responde las preguntas con las opciones indicadas. Gracias")
-document.write("Bienvenido, vamos a cotizar el seguro de tu vehículo.", "<br>", "Responde las preguntas con las opciones indicadas. Gracias", "<br><br>")
+document.write("Bienvenido, vamos a cotizar el seguro de tu vehículo.", "<br>","<br>", "Responde las preguntas con las opciones indicadas. Gracias","<br>")
+
+//variables
+
+const rcAuto = 1900.0
+const rcMoto = 350
+const tasaAutoA = 0
+const tasaAutoB = 0.004
+const tasaAutoC = 0.007
+const tasaAutoCFULL = 0.002
+const tasaMotoA = 0
+const tasaMotoB = 0.001
+const tasaMotoC = 0.002
+
+
 
 //funcion para cotizar
 
-function cotizarSeguro(){
-    let autooMoto = prompt("¿Quiere cotizar un auto o una moto? auto/moto");
-    if (autooMoto == "auto") {
-        let tercerosAuto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia");
-        if (tercerosAuto == "terceros") {
-            console.log("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
-            document.write("La responsabilidad Civil de un automóvil tiene un costo actual de $1900. Para contratar le pedimos que se comunique al 3854855796")
+function cotizarSeguro() {
+    let autooMoto = prompt("¿Quiere cotizar un auto o una moto? Auto/Moto");
+    let autooMotoLower = autooMoto.toLowerCase();
+    if (autooMotoLower == "auto") {
+        let tercerosAuto = prompt("Para cotizar Responsabilidad Civil, ingrese A.\nPara cotizar RC + Incendio, Robo/Hurto Total, ingrese B.\nPara Para cotizar RC + Incendio, Robo/Hurto Total + Destruccion Total, ingrese C.\nPara Para cotizar RC + Incendio, Robo/Hurto Total + Destruccion Total + Granizo y Adicionales, ingrese CFULL.");
+        let tercerosAutoLower = tercerosAuto.toLowerCase();
+        if (tercerosAutoLower == "a") {
+            console.log("La responsabilidad Civil de un automóvil tiene un costo actual de $" + rcAuto)
+            document.write("<br>","La responsabilidad Civil de un automóvil tiene un costo actual de $" + rcAuto)
         }
-        if (tercerosAuto == "amplia") {
-            console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+        else if (tercerosAutoLower == "b") {
+            let sumaAsegurada = prompt("Ingrese el valor actual de mercado de su vehiculo")
+            let costoAutoB = (rcAuto) + (sumaAsegurada * tasaAutoB)
+            console.log("El costo de una cobertura B, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoB.toFixed(2) + "mensual.")
+            document.write("<br>","El costo de una cobertura B, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoB.toFixed(2) + "mensual.")
         }
+        else if (tercerosAutoLower == "c") {
+            let sumaAsegurada = prompt("Ingrese el valor actual de mercado de su vehiculo")
+            let costoAutoC = (rcAuto) + (sumaAsegurada * tasaAutoC)
+            console.log("El costo de una cobertura C, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoC.toFixed(2) + "mensual.")
+            document.write("<br>","El costo de una cobertura C, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoC.toFixed(2) + "mensual.")
+        }
+        else if (tercerosAutoLower == "cfull") {
+            let sumaAsegurada = prompt("Ingrese el valor actual de mercado de su vehiculo")
+            let costoAutoCFULL = (rcAuto) + (sumaAsegurada * tasaAutoCFULL)
+            console.log("El costo de una cobertura CFull, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoCFULL.toFixed(2) + "mensual.")
+            document.write("<br>","El costo de una cobertura CFull, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoAutoCFULL.toFixed(2) + "mensual.")
+        }
+        else {
+            console.log("Lo siento, no puedo entender su respuesta. Por favor, responda con la letra de la cobertura elegida. A,B,C,CFfull.");
+            document.write("<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con letra de la cobertura elegida. A,B,C,CFull.")
+
+        }
+
     }
-    if (autooMoto == "moto") {
-        let tercerosMoto = prompt("¿Quiere un seguro solo de Responsabilidad Civil o una cobertura mas amplia? terceros/amplia")
-        if (tercerosMoto == "terceros") {
-            console.log("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
-            document.write("La responsabilidad Civil de un motovehículos tiene un costo actual de $450. Para contratar le pedimos que se comunique al 3854855796")
+    else if (autooMotoLower == "moto") {
+        let tercerosMoto = prompt("Para cotizar Responsabilidad Civil, ingrese A.\nPara cotizar RC + Incendio, Robo/Hurto Total, ingrese B.\nPara Para cotizar RC + Incendio, Robo/Hurto Total + Destruccion Total, ingrese C.");
+        let tercerosMotoLower = tercerosMoto.toLowerCase();
+        if (tercerosMotoLower == "a") {
+            console.log("La responsabilidad Civil de una moto tiene un costo actual de $" + rcMoto)
+            document.write("<br>","La responsabilidad Civil de una moto tiene un costo actual de $" + rcMoto)
         }
-        if (tercerosMoto == "amplia") {
-            console.log("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
-            document.write("Para hacer una cobertura amplia le pedimos que se comunique por teléfono o por Whatsapp al 3854855796")
+        else if (tercerosMotoLower == "b") {
+            let sumaAsegurada = prompt("Ingrese el valor actual de mercado de su vehiculo")
+            let costoMotoB = (rcMoto) + (sumaAsegurada * tasaMotoB)
+            console.log("El costo de una cobertura B, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoMotoB.toFixed(2) + "mensual.")
+            document.write("<br>","El costo de una cobertura B, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoMotoB.toFixed(2) + "mensual.")
         }
+        else if (tercerosMotoLower == "c") {
+            let sumaAsegurada = prompt("Ingrese el valor actual de mercado de su vehiculo")
+            let costoMotoC = (rcMoto) + (sumaAsegurada * tasaMotoC)
+            console.log("El costo de una cobertura C, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoMotoC.toFixed(2) + "mensual.")
+            document.write("<br>","El costo de una cobertura C, con una Suma Asegurada de $" + sumaAsegurada + " es de $" + costoMotoC.toFixed(2) + "mensual.")
+        }
+        else {
+            console.log("Lo siento, no puedo entender su respuesta. Por favor, responda con la letra de la cobertura elegida. A,B,C.");
+            document.write("<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con letra de la cobertura elegida. A,B,C.")
+
+        }
+
+    }
+    else {
+        console.log("Lo siento, no puedo entender su respuesta. Por favor, responda con auto o moto, sino comuniquese a nuestro whatsapp.");
+        document.write("<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con auto o moto, sino comuniquese a nuestro whatsapp.")
     }
 
 }
+
 
 
 //preguntas al usuario que tienen seguro
 
 let tieneSeguro
+let tieneSeguroLower
 
-do { tieneSeguro = prompt("Bienvenido. ¿Su vehículo tiene contratado un seguro? si/no");}
-while (tieneSeguro !== "si" && tieneSeguro !== "no");
+do {
+    tieneSeguro = prompt("Bienvenido. ¿Su vehículo tiene contratado un seguro? Si/No");
+    tieneSeguroLower = tieneSeguro.toLowerCase();
+}
+while (tieneSeguroLower !== "si" && tieneSeguroLower !== "no");
 
-if (tieneSeguro == "si") {
-    let quiereComparar = prompt("¿Desea cotizar su vehículo? si/no");
-    if (quiereComparar == "no") {
+
+if (tieneSeguroLower == "si") {
+    let quiereComparar = prompt("¿Desea cotizar su vehículo? Si/No");
+    let quiereCompararLower = quiereComparar.toLowerCase();
+    if (quiereCompararLower == "no") {
         alert("Gracias por visitarnos! Dudas comentarios al 3854855796");
     }
-    if (quiereComparar == "si") { cotizarSeguro()
+    if (quiereCompararLower == "si") {
+        cotizarSeguro()
     }
 }
 
 //preguntas al usuario cuando no tiene seguro
 
-else if (tieneSeguro == "no") {
+else if (tieneSeguroLower == "no") {
     quiereComparar = prompt("¿Desea cotizar su vehículo? si/no");
     if (quiereComparar == "no") {
         alert("Gracias por visitarnos! Dudas comentarios al 3854855796");
     }
-    if (quiereComparar == "si") { cotizarSeguro()
+    if (quiereComparar == "si") {
+        cotizarSeguro()
     }
 } else {
     console.log("Lo siento, no puedo entender su respuesta. Por favor, responda con las opciones permitidas");
-    document.write("<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con las opciones permitidas")
+    document.write("<br>", "<br>", "Lo siento, no puedo entender su respuesta. Por favor, responda con las opciones permitidas")
 }
 
 
