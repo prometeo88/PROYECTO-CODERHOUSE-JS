464e8828c0ae49071853020b87376f6e7b79da1c
Federico <f.ullua@gmail.com>
2023-04-14 23:33:57 -0300

    estilos y cambios de nombres

--- js/weather.js
+++ js/weather.js
@@ -8,11 +8,17 @@ fetch(url)
         const temperaturaMaxima = data.DailyForecasts[0].Temperature.Maximum.Value;
         const temperaturaMinima = data.DailyForecasts[0].Temperature.Minimum.Value;
         const temperaturaResumen = data.Headline.Text;
+        const temperaturaEnlace = data.Headline.Link;
+
         const htmlMaxima = document.getElementById("maxima")
         const htmlMinima = document.getElementById("minima")
         const htmlResumen = document.getElementById("resumen")
+        const htmlLink = document.getElementById("weatherEnlace")
+        
         htmlMaxima.textContent = `La temperatura máxima esperada para hoy es ${temperaturaMaxima} °C`
         htmlMinima.textContent = `La temperatura mínima esperada para hoy es ${temperaturaMinima} °C`
         htmlResumen.textContent = `${temperaturaResumen}`
+        htmlLink.setAttribute("href", temperaturaEnlace );
+
     } )
 	.catch(err => console.error(err));
